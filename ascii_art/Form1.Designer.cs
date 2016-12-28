dd1c87e5c13587fd571b97da3fe5cb5e50dbb682
Russell Schoen <rschoen@fastmail.fm>
2016-12-28 10:23:05 -0600

    added solution

---
+++ ascii_art/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace ascii_art
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBxCharH = new System.Windows.Forms.TextBox();
+            this.txtBxCharW = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBxScale = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnScaleShort = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnScaleLong = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(411, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadImageToolStripMenuItem,
+            this.exportImageToolStripMenuItem,
+            this.closeImageToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // loadImageToolStripMenuItem
+            // 
+            this.loadImageToolStripMenuItem.Name = "loadImageToolStripMenuItem";
+            this.loadImageToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.loadImageToolStripMenuItem.Text = "&Load Image";
+            this.loadImageToolStripMenuItem.Click += new System.EventHandler(this.loadImageToolStripMenuItem_Click);
+            // 
+            // exportImageToolStripMenuItem
+            // 
+            this.exportImageToolStripMenuItem.Name = "exportImageToolStripMenuItem";
+            this.exportImageToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exportImageToolStripMenuItem.Text = "&Export Image";
+            this.exportImageToolStripMenuItem.Click += new System.EventHandler(this.exportImageToolStripMenuItem_Click);
+            // 
+            // closeImageToolStripMenuItem
+            // 
+            this.closeImageToolStripMenuItem.Name = "closeImageToolStripMenuItem";
+            this.closeImageToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.closeImageToolStripMenuItem.Text = "&Close Image";
+            this.closeImageToolStripMenuItem.Click += new System.EventHandler(this.closeImageToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.aboutToolStripMenuItem1});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // aboutToolStripMenuItem1
+            // 
+            this.aboutToolStripMenuItem1.Name = "aboutToolStripMenuItem1";
+            this.aboutToolStripMenuItem1.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem1.Text = "&About";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Letter Height:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(212, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Letter Width:";
+            // 
+            // txtBxCharH
+            // 
+            this.txtBxCharH.Location = new System.Drawing.Point(98, 27);
+            this.txtBxCharH.Name = "txtBxCharH";
+            this.txtBxCharH.Size = new System.Drawing.Size(31, 20);
+            this.txtBxCharH.TabIndex = 2;
+            // 
+            // txtBxCharW
+            // 
+            this.txtBxCharW.Location = new System.Drawing.Point(293, 27);
+            this.txtBxCharW.Name = "txtBxCharW";
+            this.txtBxCharW.Size = new System.Drawing.Size(31, 20);
+            this.txtBxCharW.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(138, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "px";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(330, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "px";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtBxScale
+            // 
+            this.txtBxScale.Location = new System.Drawing.Point(15, 97);
+            this.txtBxScale.Name = "txtBxScale";
+            this.txtBxScale.Size = new System.Drawing.Size(384, 20);
+            this.txtBxScale.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 81);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Color Scale:";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(163, 123);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(95, 20);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "Reset options";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(304, 123);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(95, 20);
+            this.btnGenerate.TabIndex = 6;
+            this.btnGenerate.Text = "Generate Text";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnScaleShort
+            // 
+            this.btnScaleShort.Location = new System.Drawing.Point(83, 74);
+            this.btnScaleShort.Name = "btnScaleShort";
+            this.btnScaleShort.Size = new System.Drawing.Size(75, 20);
+            this.btnScaleShort.TabIndex = 7;
+            this.btnScaleShort.Text = "Short";
+            this.btnScaleShort.UseVisualStyleBackColor = true;
+            this.btnScaleShort.Click += new System.EventHandler(this.btnScaleShort_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(15, 123);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(95, 20);
+            this.btnOpen.TabIndex = 6;
+            this.btnOpen.Text = "Open Image";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnScaleLong
+            // 
+            this.btnScaleLong.Location = new System.Drawing.Point(163, 74);
+            this.btnScaleLong.Name = "btnScaleLong";
+            this.btnScaleLong.Size = new System.Drawing.Size(75, 20);
+            this.btnScaleLong.TabIndex = 7;
+            this.btnScaleLong.Text = "Long";
+            this.btnScaleLong.UseVisualStyleBackColor = true;
+            this.btnScaleLong.Click += new System.EventHandler(this.btnScaleLong_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(411, 164);
+            this.Controls.Add(this.btnScaleLong);
+            this.Controls.Add(this.btnScaleShort);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtBxScale);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBxCharW);
+            this.Controls.Add(this.txtBxCharH);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Image to ASCII Generator";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem closeImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBxCharH;
+        private System.Windows.Forms.TextBox txtBxCharW;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBxScale;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnScaleShort;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnScaleLong;
+    }
+}
+
---
+++ ascii_art/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ascii_art
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap bit;
+        private string output;
+        private string filename;
+        public Form1()
+        {
+            InitializeComponent();
+            ResetTextBoxes();
+        }
+
+        private void ResetTextBoxes()
+        {
+            txtBxCharH.Text = "9";
+            txtBxCharW.Text = "5";
+            SetScaleShort();
+        }
+
+        private void SetScaleShort()
+        {
+            txtBxScale.Text = "@%#*+=-:. ";
+        }
+        private void SetScaleLong()
+        {
+            txtBxScale.Text = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\" ^`'. ";
+        }
+        
+
+
+        private void loadImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenImage();
+        }
+        private void OpenImage()
+        {
+            openFileDialog1.Filter = "Supported Files (*.bmp, *.jpg, *.png, *.tiff)|*.bmp;*.jpg;*.png;*.tiff";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string ext = Path.GetExtension(openFileDialog1.FileName);
+                filename = Path.GetFileNameWithoutExtension(openFileDialog1.FileName);
+
+                if (ext == ".bmp" || ext == ".jpg" || ext == ".png" || ext == ".tiff")
+                {
+                    bit = new Bitmap(openFileDialog1.FileName);
+                }
+            }
+
+        }
+
+        private bool bmpToText()
+        {
+            int charH, charW;
+            if (int.TryParse(txtBxCharH.Text, out charH) == false)
+            {
+                MessageBox.Show("The text boxes must contain numbers.");
+                return false;
+            }
+            if (int.TryParse(txtBxCharW.Text, out charW) == false)
+            {
+                MessageBox.Show("The text boxes must contain numbers.");
+                return false;
+            }
+            output = "";
+            string greyscale = txtBxScale.Text;
+            //begin outer loop. searches in 5x7 blocks
+            for (int yo = 0; yo < bit.Height; yo += charH)
+            {
+                if (yo + charH < bit.Height) //to avoid going over boundaries
+                {
+                    for (int xo = 0; xo < bit.Width; xo += charW)
+                    {
+                        if (xo + charW < bit.Width)
+                        {
+                            //begin inner loop. searches every pixel in a single block.
+                            int avgTotal = 0;
+                            double scaleNumber;
+                            for (int yi = 0; yi < charH; yi++)
+                            {
+                                for (int xi = 0; xi < charW; xi++)
+                                {
+                                    int x = xi + xo;
+                                    int y = yi + yo;
+                                    Color newColor = bit.GetPixel(x, y);
+                                    int avg = (newColor.R + newColor.G + newColor.B) / 3;
+                                    avgTotal += avg;
+
+                                }
+                            }
+                            double avgFinal = avgTotal / (charH * charW);
+                            double avgFinalPercent = avgFinal / 255;
+                            scaleNumber = avgFinalPercent * greyscale.Length;
+                            output += greyscale[(int)scaleNumber];
+                        }
+                    }
+                    output += "\r\n";
+                }
+            }
+            return true;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void testToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void closeImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void exportImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ExportAscii();
+        }
+
+        private void ExportAscii()
+        {
+            Stream stream;
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.FileName = filename + ".txt";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((stream = saveFileDialog1.OpenFile()) != null)
+                {
+                    StreamWriter sw;
+                    if (bmpToText())
+                    {
+                        sw = new StreamWriter(stream);
+                        sw.WriteLine(output);
+                        sw.Close();
+                    }
+                    stream.Close();
+                }
+            }
+            MessageBox.Show("Successfully Exported!");
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenImage();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ExportAscii();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            ResetTextBoxes();
+        }
+
+        private void btnScaleShort_Click(object sender, EventArgs e)
+        {
+            SetScaleShort();
+        }
+
+        private void btnScaleLong_Click(object sender, EventArgs e)
+        {
+            SetScaleLong();
+        }
+    }
+}
